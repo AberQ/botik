a004663f00d661d85a828a917bd72f9544e7163f
AberQ <egor.master2017@gmail.com>
2025-01-24 00:23:39 +0500

    Исправил ошибку из-за которой не совершались запросы по подпискам

--- main.py
+++ main.py
@@ -1,25 +1,22 @@
-from asyncio import run, create_task
+from asyncio import create_task
 from apscheduler.schedulers.asyncio import AsyncIOScheduler
 from fastapi import FastAPI
-from pydantic import BaseModel
-from sqlalchemy.ext.asyncio import AsyncSession, create_async_engine
-from sqlalchemy.orm import sessionmaker
 from routers.products import router, update_product_details
-from database import SessionLocal, engine, init_db
+from database import init_db
+
 app = FastAPI()
 
 app.include_router(router)
 
-
-
 scheduler = AsyncIOScheduler()
 
-
-def update_product_details_wrapper():
+async def update_product_details_wrapper():
+    # Создаем асинхронную задачу для обновления данных
     create_task(update_product_details())
 
 def start_scheduler():
-    scheduler.add_job(update_product_details_wrapper, 'interval', seconds=1800)
+    # Добавляем задачу в планировщик с интервалом 30 секунд
+    scheduler.add_job(update_product_details_wrapper, 'interval', seconds=30)
     scheduler.start()
 
 @app.on_event("startup")
